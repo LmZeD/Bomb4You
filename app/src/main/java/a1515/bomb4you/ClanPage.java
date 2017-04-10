86ea05fb9249e586cef2992876d911099c183ab5
Linas <lmzedas@gmail.com>
2017-04-10 20:58:18 +0300

    First commit

--- app/src/main/java/a1515/bomb4you/ClanPage.java
+++ app/src/main/java/a1515/bomb4you/ClanPage.java
@@ -9,8 +9,6 @@ public class ClanPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_clan_page);
-
-
     }
 
 
